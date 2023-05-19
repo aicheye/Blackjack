abc8b0f1f2510749c8da440af99a97f762ad094d
Sean Yang <seanxxyang@gmail.com>
2023-05-18 20:11:46 -0400

    support prefix in centeredinputs.py

--- centeredinputs.py
+++ centeredinputs.py
@@ -4,6 +4,7 @@ import readchar
 input_field = ""
 instructions = ""
 user_in = ""
+prefix = "$"
 
 
 def main(cols):
@@ -13,7 +14,7 @@ def main(cols):
 
     print("╚" + ("╤" + "═" * 21 + "╤").center(cols - 2, "═") + "╝")
     print(("│" + instructions.center(21) + "│").center(cols))
-    print(("│" + input_field.center(21) + "│").center(cols))
+    print(("│" + prefix + input_field.center(21) + "│").center(cols))
     print(("╰" + "─" * 21 + "╯").center(cols))
 
     key = readchar.readkey()
@@ -21,8 +22,7 @@ def main(cols):
     if key == readchar.key.ENTER:
         user_in = input_field
         return True
-    elif key == readchar.key.BACKSPACE:  # Backspace key
-        print(input_field)
+    elif key == readchar.key.BACKSPACE:
         input_field = input_field[:-1]
     elif len(input_field) < 15:
         input_field += key
